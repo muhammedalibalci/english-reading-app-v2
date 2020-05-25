496c87eaedbda53222e34d63815b894a66c7dd13
Muhammed Ali <40212997+muhammedalibalci@users.noreply.github.com>
2020-05-25 22:47:23 +0300

    Added a new scroll

--- src/screens/Reading.js
+++ src/screens/Reading.js
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from 'react'
-import { View, Text, StyleSheet, ScrollView, Image } from 'react-native'
+import { View, Text, StyleSheet, FlatList, Image, Dimensions } from 'react-native'
 import { SelectableText } from "@astrocoders/react-native-selectable-text";
 import Icon from 'react-native-vector-icons/FontAwesome'
 import SettingModal from '../components/SettingModal';
 import { Footer } from '../components/Footer';
 import { useFocusEffect } from '@react-navigation/native';
 import { BaseManager } from '../utils/SqliteDb'
+import Pagination from 'react-native-pagination';//{Icon,Dot} also available
+import { BookPagesScroll } from '../components/BookPagesScroll';
+import { ScrollView } from 'react-native-gesture-handler';
+
 var db = new BaseManager()
 export const Reading = ({ route }) => {
 
     const { book } = route.params
+    const [bookContent, setBookContent] = useState([])
     const [fontSize, setFontSize] = useState(18)
     const [justify, setJustify] = useState('auto')
     const [darkMode, setDarkMode] = useState('white')
@@ -29,11 +34,33 @@ export const Reading = ({ route }) => {
                 if (res === 1) setIsEnabledFavorite(true)
             }).catch(er => {
                 console.log("Error", er);
-    
+
             })
+
+            const bookLenght = book.content.length
+            const wordsNumbersInAPage = 690
+
+            const pageSize = bookLenght / wordsNumbersInAPage
+            let startText = 0
+            let endText = wordsNumbersInAPage
+            let gatheredContents = []
+            for (let index = 0; index < pageSize; index++) {
+                let dividedText = book.content.substring(startText, endText)
+                startText = endText
+                endText = endText * 2
+                console.log(dividedText);
+                
+                gatheredContents.push({ dividedText })
+            }
+
+            setBookContent([...gatheredContents])
+
+
         }, [])
     );
     const onClickSetting = () => {
+        console.log(bookContent);
+
         setVisibleSetting(!visibleSetting)
     }
 
@@ -76,9 +103,9 @@ export const Reading = ({ route }) => {
             })
 
         }
-        else{
+        else {
             db.deleteData(book.title).then(res => {
-                
+
             }).catch(er => {
                 console.log(er);
             })
@@ -109,46 +136,25 @@ export const Reading = ({ route }) => {
         setFooterVisible(true)
     }
     return (
-        <View style={
-            [
-                styles.container,
-                {
-                    backgroundColor: darkMode,
-
-                }
-            ]
-        }>
+        <View style={[styles.container,{backgroundColor: darkMode,}]}>
             <View style={{ flexDirection: 'row' }}>
                 <Image source={{ uri: book.image || book.imageurl }} style={styles.image} />
                 <View style={{ flex: 0.9 }}>
-                    <Text  selectable style={[styles.title, { color: color }]}>{book.title} </Text>
+                    <Text selectable style={[styles.title, { color: color }]}>{book.title} </Text>
                 </View>
                 <View style={{ flex: 0.1, marginTop: 10 }}>
                     <Icon onPress={onClickSetting} name="font" size={24} color="orange" />
                 </View>
             </View>
-            <View >
-
-                <ScrollView >
-                    <View style={styles.lineStyle} />
-
-                    <SelectableText
-                        style={[
-                            styles.text,
-                            {
-                                fontSize: fontSize,
-                                textAlign: justify,
-                                color: color
-                            }
-                        ]
-
-                        }
-                        menuItems={["Translate", "Paint"]}
-                        onSelection={({ eventType, content, selectionStart, selectionEnd }) => { onTextPress(eventType, content) }}
-                        value={book.content}
-                    />
-                </ScrollView>
-            </View>
+            <ScrollView>
+                <View style={styles.lineStyle} />
+                <BookPagesScroll
+                    page={bookContent}
+                    onTextPress={onTextPress}
+                    fontSize={fontSize}
+                    justify={justify}
+                    color={color} />
+            </ScrollView>
             <SettingModal
                 onClickTextZoom={onClickTextZoom}
                 onClickTextUnZoom={onClickTextUnZoom}
@@ -165,7 +171,8 @@ export const Reading = ({ route }) => {
 }
 const styles = StyleSheet.create({
     container: {
-        padding: 10,
+        paddingTop: 10,
+        paddingLeft:5
     },
     title: {
         fontSize: 28,
@@ -180,7 +187,6 @@ const styles = StyleSheet.create({
     lineStyle: {
         borderBottomColor: 'gray',
         borderBottomWidth: 1,
-        marginBottom: 25
     },
     image: {
         height: 50,
